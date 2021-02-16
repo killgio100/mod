946857c086471220453b51934ae3571f43e62b88
killgio100 <79112809+killgio100@users.noreply.github.com>
2021-02-16 14:50:32 +0100

    qwertyuj

--- src/main/java/net/mcreator/prova/block/TopazioOreBlock.java
+++ src/main/java/net/mcreator/prova/block/TopazioOreBlock.java
@@ -82,7 +82,7 @@ public class TopazioOreBlock extends ProvaModElements.ModElement {
 				if (blockAt.getBlock() == Blocks.STONE.getDefaultState().getBlock())
 					blockCriteria = true;
 				return blockCriteria;
-			}), block.getDefaultState(), 2)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(3, 1, 1, 20))));
+			}), block.getDefaultState(), 2)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(2, 0, 0, 15))));
 		}
 	}
 }
