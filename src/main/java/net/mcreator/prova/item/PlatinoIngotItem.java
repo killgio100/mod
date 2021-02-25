28cc5a6b266ad1785f9832daeeb6977adfe9aff1
killgio100 <79112809+killgio100@users.noreply.github.com>
2021-02-25 19:46:26 +0100

    11

---
+++ src/main/java/net/mcreator/prova/item/PlatinoIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.prova.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.prova.ProvaModElements;
+
+@ProvaModElements.ModElement.Tag
+public class PlatinoIngotItem extends ProvaModElements.ModElement {
+	@ObjectHolder("prova:platino_ingot")
+	public static final Item block = null;
+	public PlatinoIngotItem(ProvaModElements instance) {
+		super(instance, 82);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("platino_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
